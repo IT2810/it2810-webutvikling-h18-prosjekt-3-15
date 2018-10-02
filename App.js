03390bc64e930db8c0fa0f86a14740ced65dbbd2
Ebba Louise Toreld Fingarsen <elfingar@stud.ntnu.no>
2018-10-02 13:55:08 +0200

    Simple button component for testing and further dev. #2

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet} from 'react-native';
+import CustomButton from './Components/customButton.js';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <CustomButton
+                    text="Click Me"
+                    onPress={() => {
+                        alert("Hi there!!!");
+                    }}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
