8acd9b96a56972f335c3b59cb970c8f74e444d1b
mortefal <morten_falstad@hotmail.com>
2018-10-16 13:39:10 +0200

    Mainpage and subject component, some bugs, pushing for pairprogramming, #1

---
+++ components/Subject.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    AsyncStorage,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+export default class Subject extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: this.props.url,
+
+        }
+    }
+
+    render() {
+        return (
+            <View key={this.props.keyCount} style={styles.subject}>
+                <Text style={styles.subjectText}>{this.props.subjectText}</Text>
+                <Icon name='arrow-right' size={15} color='blue'/>
+                <TouchableOpacity onPress={this.props.deleteMethod} style={styles.subjectDelete}>
+                    <Text style={styles.subjectDeleteText}>Delete</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+}
+const styles = StyleSheet.create({
+    subject: {
+        position: 'relative',
+        padding: 20,
+        paddingRight: 100,
+        borderBottomWidth: 2,
+        borderBottomColor: '#ededed'
+    },
+    subjectText: {
+        paddingLeft: 20,
+        borderLeftWidth: 15,
+        paddingRight: 5,
+        borderLeftColor: '#4286f4',
+        opacity: 0.7,
+    },
+    subjectDelete: {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f44141',
+        padding: 10,
+        top: 15,
+        bottom: 15,
+        right: 10
+    },
+    subjectDeleteText: {
+        color: 'white'
+    },
+});
