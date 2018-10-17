a6199052b3ec913480d0d64ceab43b5c7958c8c1
jonasbgi <jonasbgi@stud.ntnu.no>
2018-10-17 15:11:54 +0200

    Moved CountDown and pausebutton into a new wrapping component

--- App.js
+++ App.js
@@ -1,35 +1,21 @@
 import React from 'react';
 import { View, StyleSheet} from 'react-native';
-import RoundButton from './Components/roundButton.js';
 import Todo from "./Components/Todo";
-import CountDown from './Components/CountDown.js';
+import CountdownComponent from "./Components/CountdownComponent";
 
 export default class App extends React.Component {
 	constructor(props){
 		super(props);
-		//Just a demo! Move timerPaused to wherever the playbutton and list-screen is
-		this.state = {
-			timerPaused: true
-		}
 	}
 
     render() {
         return (
             <View style={styles.container}>
 				{/*Countdown screen- Move to list-view when ready*/}
-                <CountDown style={styles.countdown}
+                <CountdownComponent
+					style={styles.countdown}
                     until={2700}
-                    timeToShow={['M', 'S']}
-					paused={this.state.timerPaused}/>
-                <RoundButton
-                    text= "play"
-                    onPress={() => {
-                        this.setState({
-							timerPaused: !this.state.timerPaused
-						});
-                        debugger;
-                    }}
-                />
+                    timeToShow={['M', 'S']}/>
                 <Todo/>
             </View>
         );
---
+++ Components/CountdownComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, StyleSheet} from 'react-native';
+import Todo from "./Components/Todo";
+import CountdownComponent from "./Components/CountdownComponent";
+
+export default class App extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				{/*Countdown screen- Move to list-view when ready*/}
+				<CountdownComponent
+					style={styles.countdown}
+					until={2700}
+					timeToShow={['M', 'S']}/>
+				<Todo/>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+		flexDirection: 'column',
+	},
+	countdown:{
+		marginTop: 30,
+	}
+});
