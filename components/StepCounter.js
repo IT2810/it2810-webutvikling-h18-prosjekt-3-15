25e789d01951147078766ac6fb6e6f7bc2eb1516
jonasbgi <jonasbgi@stud.ntnu.no>
2018-10-18 16:42:16 +0200

    Added stepCounter component

---
+++ components/StepCounter.js
@@ -0,0 +1,62 @@
+import { Component } from 'react';
+import { Pedometer } from "expo";
+
+export default class StepCounter extends Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			startDate: new Date(),
+			endDate: new Date,
+			stepCount: 0
+		}
+	}
+
+
+
+	resetSteps(){
+		this.setState({
+			startDate: new Date()
+		});
+	}
+
+	componentDidMount() {
+		this.resetSteps();
+		this._subscribe();
+	}
+
+	componentWillUnmount() {
+		this._unsubscribe();
+	}
+
+	_subscribe = () => {
+		this._subscription = Pedometer.watchStepCount(result => {
+			// If resetStepCount is true, we'll set the current stepCount to what is current.
+			Pedometer.getStepCountAsync(this.state.startDate, new Date()).then(
+				result => {
+					this.setState({
+						stepCount: result.steps
+					});
+				}
+			);
+
+
+		});
+	};
+	_unsubscribe = () => {
+		this._subscription && this._subscription.remove();
+		this._subscription = null;
+	};
+
+
+
+	render() {
+		return(
+			<View>
+				<Text>Steps: {this.state.stepCount}</Text>
+			</View>
+		)
+	}
+
+};
